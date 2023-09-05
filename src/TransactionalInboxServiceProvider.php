9bb586952fb32b97891382de545d7b62fd617759
Predrag Rakonjac <predrag.rakonjac@quantox.com>
2023-09-05 23:33:42 +0200

    add laravel service provider

---
+++ src/TransactionalInboxServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace ShowersAndBs\TransactionalInbox;
+
+use Illuminate\Support\ServiceProvider;
+
+class TransactionalInboxServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
